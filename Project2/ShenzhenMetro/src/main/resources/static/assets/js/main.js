de539029caccf318cb238ed20106bb1c173d5e5a
fanxychild <100819203+Layheng-Hok@users.noreply.github.com>
2024-07-04 17:24:56 +0700

    Add opening animation

--- Project2/ShenzhenMetro/src/main/resources/static/assets/js/main.js
+++ Project2/ShenzhenMetro/src/main/resources/static/assets/js/main.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+    /**
+     * Preloader
+     */
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                preloader.classList.add('loaded');
+            }, 1000);
+            setTimeout(() => {
+                preloader.remove();
+            }, 2000);
+        });
+    }
 
     /**
      * Mobile nav toggle
